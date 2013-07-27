36472d14892a2616b4340736b6bae2c0472aefdd
Stephen Mathieson <smath23@gmail.com>
2013-07-27 19:23:00 -0400

    two vars (easy check for custom options)

--- examples/basic/hello-world.js
+++ examples/basic/hello-world.js
@@ -1,6 +1,6 @@
 exports.helloWorld = function () {
-  var hello = require('./hello').hello,
-      world = require('./world').world;
+  var hello = require('./hello').hello;
+  var world = require('./world').world;
 
   return hello + ' ' + world;
 };
