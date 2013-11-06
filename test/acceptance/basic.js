32f85456f9241498c8dceadfbba0b697a569828e
Stephen Mathieson <smath23@gmail.com>
2013-11-05 19:55:59 -0500

    initial synchronous API
    see #6

--- test/acceptance/basic.js
+++ test/acceptance/basic.js
@@ -16,12 +16,7 @@ var opts = obfuscator.options([
   path.join(EXAMPLES, 'basic', 'hello-world.js'),
   true);
 
-obfuscator.obfuscator(opts, function (err, code) {
-  if (err) {
-    throw err;
-  }
-
-  var file = path.join(EXAMPLES, 'basic', 'obfuscated.js');
-  fs.writeFileSync(file, code);
-  assert.equal(require(file).helloWorld(), 'hello world');
-});
+var code = obfuscator.sync(opts);
+var file = path.join(EXAMPLES, 'basic', 'obfuscated.js');
+fs.writeFileSync(file, code);
+assert.equal(require(file).helloWorld(), 'hello world');
