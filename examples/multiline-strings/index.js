e60a4bfa273ae4d17b827019b90133bfe9c0e5d4
Stephen Mathieson <smath23@gmail.com>
2014-04-03 10:16:51 -0400

    test: Verify multiline strings

---
+++ examples/multiline-strings/index.js
@@ -0,0 +1,10 @@
+
+var str =
+  'hello\
+  world\
+  and\
+  stuff';
+
+exports.fn = function () {
+  return str;
+}
---
+++ test/acceptance/multiline-strings.js
@@ -0,0 +1,21 @@
+
+
+var path = require('path');
+var assert = require('better-assert');
+var fs = require('fs');
+var obfuscator = require('../..');
+
+var root = path.join(__dirname, '..', '..', 'examples', 'multiline-strings');
+var entry = path.join(root, 'index.js');
+var files = [entry];
+
+var opts = new obfuscator.Options(files, root, entry, true);
+
+obfuscator(opts, function (err, code) {
+  if (err) throw err;
+  var file = path.join(root, 'obfuscated.js');
+  fs.writeFileSync(file, code);
+
+  var module = require(file);
+  assert('hello  world  and  stuff' == module.fn());
+});
